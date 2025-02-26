2e3d072b015b96d7c764710a18c7eccc99ecf08d
kingwhangzang <soyun011112@sookmyung.ac.kr>
2025-02-26 16:47:37 +0900

    first commit

---
+++ src/test/java/com/example/jemyeonson/JemyeonsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jemyeonson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JemyeonsonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

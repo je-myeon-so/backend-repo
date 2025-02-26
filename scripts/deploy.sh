375b2c80f7e6211522a53b00853070357b7e21d4
kwakyongwoo <43498544+kwakyongwoo@users.noreply.github.com>
2025-02-26 21:30:22 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,6 +4,6 @@ sudo docker rmi kkolbuyw/jemyeonso-spring:latest
 
 sudo docker pull kkolbuyw/jemyeonso-spring:latest
 
-docker run -d -p 8080:8080 --env-file=env_list.txt --name app kkolbuyw/jemyeonso-spring:latest
+docker run -d -p 8080:8080 --name app kkolbuyw/jemyeonso-spring:latest
 
 docker rmi -f $(docker images -f "dangling=true" -q) || true
